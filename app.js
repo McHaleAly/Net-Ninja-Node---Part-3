5339df931928e11e6cf4e591d3be5339863aa854
AlysonAtwell <123188201+McHaleAly@users.noreply.github.com>
2023-02-21 21:12:48 -0500

    commit code

---
+++ app.js
@@ -0,0 +1,34 @@
+const express = require('express');
+
+//express app
+const app = express(); //this is the app
+
+//rester view engine
+app.set('view engine', 'ejs');
+
+// listen for requests this is the server
+app.listen(3000);
+
+app.get('/', (req, res) => {
+    const blogs = [
+        {title: 'Yoshi finds eggs', snippet: 'Lorem ipsum dolor sit amet consectetur'},
+        {title: 'mario finds star', snippet: 'Lorem ipsum dolor sit amet consectetur'},
+        {title: 'How to defeat browser', snippet: 'Lorem ipsum dolor sit amet consectetur'},
+    ];
+    res.render('index',{title: 'Home', blogs});
+});
+
+app.get('/about', (req, res) => {
+    res.render('about',{title: 'About'});
+ });
+
+ 
+app.get('/blogs/create', (req, res) => {
+    res.render('create',{title: 'Create a new Blog '});
+});
+
+
+ //404 page
+app.use((req,res)=>{
+    res.status(404).render('404',{title: '404'}) 
+});
