fa2ff3f60ab7c1db77a65073e1f8d3484241caa2
AlysonAtwell <123188201+McHaleAly@users.noreply.github.com>
2023-02-22 15:49:30 -0500

    checked all ejs pages

--- app.js
+++ app.js
@@ -1,32 +1,33 @@
 const express = require('express');
 
-//express app
-const app = express(); //this is the app
+// express app
+const app = express();
 
-//rester view engine
-app.set('view engine', 'ejs');
-
-// listen for requests this is the server
+// listen for requests
 app.listen(3000);
 
+// register view engine
+app.set('view engine', 'ejs');
+// app.set('views', 'myviews');
+
 app.get('/', (req, res) => {
-    const blogs = [
-       
-    ];
-    res.render('index',{title: 'Home', blogs});
+  const blogs = [
+    {title: 'Yoshi finds eggs', snippet: 'Lorem ipsum dolor sit amet consectetur'},
+    {title: 'Mario finds stars', snippet: 'Lorem ipsum dolor sit amet consectetur'},
+    {title: 'How to defeat bowser', snippet: 'Lorem ipsum dolor sit amet consectetur'},
+  ];
+  res.render('index', { title: 'Home', blogs });
 });
 
 app.get('/about', (req, res) => {
-    res.render('about',{title: 'About'});
- });
-
- 
-app.get('/blogs/create', (req, res) => {
-    res.render('create',{title: 'Create a new Blog '});
+  res.render('about', { title: 'About' });
 });
 
+app.get('/blogs/create', (req, res) => {
+  res.render('create', { title: 'Create a new blog' });
+});
 
- //404 page
-app.use((req,res)=>{
-    res.status(404).render('404',{title: '404'}) 
+// 404 page
+app.use((req, res) => {
+  res.status(404).render('404', { title: '404' });
 });
